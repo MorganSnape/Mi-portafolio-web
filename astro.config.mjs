501219770dac234a4da4385c994dd89dbc72f53e
Gabriela Ferrer <gabriellangeles@outlook.com>
2025-04-18 13:22:54 -0500

    Updated Astro and Tailwind dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-import tailwind from '@astrojs/tailwind';
 
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    icon(),
-    tailwind()
-  ]
+  integrations: [icon()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
