a01d5dd3a7480cff697c314689a2308e292f1a4d
Gabriela Ferrer <gabriellangeles@outlook.com>
2025-04-18 12:44:46 -0500

    enabled tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
+import tailwind from '@astrojs/tailwind';
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    icon(), 
+    icon(),
+    tailwind()
   ]
 });
