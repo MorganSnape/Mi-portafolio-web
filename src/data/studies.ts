272a167050059f5c49ce5100b1ef4f6b85c6e463
morgansnape <gabriellangeles@outlook.com>
2025-12-04 13:04:00 -0500

    Actualiación de datos (Estudio y Experiencia)

--- src/data/studies.ts
+++ src/data/studies.ts
@@ -3,67 +3,74 @@ import type { Studies } from "@/models";
 
 export const estudios: Studies[] = [
   {
-  title: "Diseño de experiencia del usuario (UX) de Google",
-  institution: "Certificado profesional de Google + Coursera",
-  descriptions: [
-    "Fundamentos del diseño de experiencia de usuario. ",
-    "Procesos de diseño, tipos de frameworks",
-    "Construir esquemas de página y prototipos. "
-  ],
-  link: "",
-  tags: ["Figma", "Investigación", "Usabilidad", "UI", "Prototipado"],
-  duration: "En curso"
-},
-{
-  title: "Programa especializado: Diseño gráfico",
-  institution: "Programa de Calarts + Coursera",
-  descriptions: [
-    "Aprendí teoría del color y composición visual.",
-    "Comunicación mediante tipografías e imágenes.",
-    "Trabajo en diseño de interfaces, gráficos en movimiento y diseño editorial."
-  ],
-  link: "",
-  tags: ["Teoría color", "Identidad marca", "Branding esencial", "Jerarquía visual"],
-  duration: "En curso"
-},
-{
-  title: "Programación en Javascript Frontend",
-  institution: "Diplomado de Oracle + Alura Latam",
-  descriptions: [
-    "Implementé componentes interactivos con JavaScript Vanilla",
-    "Apliqué principios de Mobile First en proyectos web",
-    "Maqueté interfaces con HTML5 y CSS3",
-    "Desarrollé prácticas con enfoque en accesibilidad"
-  ],
-  link: "https://drive.google.com/file/d/1KhzrS3UDrZL9FtRwEp27Hkt29dVMxF_q/view?usp=sharing",
-  tags: ["HTML", "CSS", "JavaScript", "React",],
-  duration: "Finalizado 01/2025"
-},
-{
-  title: "Fundamentos de UX / UI",
-  institution: "Universidad Católica de Pereira",
-  descriptions: [
-    "Comprendí principios básicos de diseño centrado en el usuario",
-    "Utilicé Figma para prototipar interfaces funcionales",
-    "Apliqué metodologías de Design Thinking en casos reales",
-    "Diseñé flujos de navegación enfocados en usabilidad"
-  ],
-  link: "https://drive.google.com/file/d/1-QLGSproo1hez7vZuZ6IewhymdjwRwHW/view?usp=sharing",
-  tags: ["UX", "UI", "Figma", "Design Thinking"],
-  duration: "Finalizado 03/2024"
-},
-{
-  title: "Introducción al UX Research y UX Writing",
-  institution: "Universidad Católica de Pereira",
-  descriptions: [
-    "Planifique investigaciones centradas en el usuario",
-    "Análisis e Interpretación de Datos",
-    "Aprendí a definir tono y voz de marca",
-    "Apliqué métodos de investigación como encuestas y testeo",
-    "Construí mapas de empatía para entender al usuario"
-  ],
-  link: "https://drive.google.com/file/d/1Hz7hTiqsr4FSG0q3E1AWKK6Fx50LFpdu/view?usp=drive_link",
-  tags: ["UX Research", "UX Writing", "Investigación", "Mapas de empatía","User persons","Storytelling"],
-  duration: "Finalizado 05/2024"
-}
+    title: "Carrera tecnológica Diseño Gráfico",
+    institution: "Servicio Nacional de Aprendizaje (SENA)",
+    descriptions: [
+      "Fundamentos del diseño gráfico, teoría del color y composición visual.",
+      "Creación de piezas gráficas para medios digitales e impresos aplicando principios de comunicación visual.",
+      "Dominio de software de diseño para la producción de material gráfico y recursos visuales.",
+      "Desarrollo de identidades visuales, branding y piezas promocionales orientadas a objetivos comunicativos.",
+      "Conceptualización de proyectos visuales integrando ilustración, diagramación y narrativa gráfica.",
+      "Aplicación de procesos creativos, metodologías de diseño y producción de contenidos visuales."
+    ],
+    link: "",
+    tags: ["Diseño Gráfico", "Medios Visuales", "Branding", "Ilustración", "Composición", "Teoría color",],
+    duration: "En curso"
+  },
+  {
+    title: "Diseño de experiencia del usuario (UX) de Google",
+    institution: "Certificado profesional de Google + Coursera",
+    descriptions: [
+      "Fundamentos del diseño de experiencia de usuario.",
+      "Procesos de diseño, tipos de frameworks.",
+      "Construir esquemas de página y prototipos.",
+      "Aplicar investigación con usuarios para identificar necesidades, problemas y oportunidades de mejora.",
+      "Crear wireframes, flujos de usuario y prototipos de baja y alta fidelidad enfocados en accesibilidad y usabilidad.",
+      "Diseñar interfaces centradas en el usuario utilizando principios heurísticos, arquitectura de información y guía de estilos.",
+      "Realizar pruebas de usabilidad, interpretar resultados y aplicar iteraciones basadas en retroalimentación real."
+    ],
+    link: "",
+    tags: ["Figma", "Investigación", "Usabilidad", "UI", "Prototipado"],
+    duration: "En curso"
+  },
+  {
+    title: "Programación en Javascript Frontend",
+    institution: "Diplomado de Oracle + Alura Latam",
+    descriptions: [
+      "Implementé componentes interactivos con JavaScript Vanilla",
+      "Apliqué principios de Mobile First en proyectos web",
+      "Maqueté interfaces con HTML5 y CSS3",
+      "Desarrollé prácticas con enfoque en accesibilidad"
+    ],
+    link: "https://drive.google.com/file/d/1KhzrS3UDrZL9FtRwEp27Hkt29dVMxF_q/view?usp=sharing",
+    tags: ["HTML", "CSS", "JavaScript", "React",],
+    duration: "Finalizado 01/2025"
+  },
+  {
+    title: "Fundamentos de UX / UI",
+    institution: "Universidad Católica de Pereira",
+    descriptions: [
+      "Comprendí principios básicos de diseño centrado en el usuario",
+      "Utilicé Figma para prototipar interfaces funcionales",
+      "Apliqué metodologías de Design Thinking en casos reales",
+      "Diseñé flujos de navegación enfocados en usabilidad"
+    ],
+    link: "https://drive.google.com/file/d/1-QLGSproo1hez7vZuZ6IewhymdjwRwHW/view?usp=sharing",
+    tags: ["UX", "UI", "Figma", "Design Thinking"],
+    duration: "Finalizado 03/2024"
+  },
+  {
+    title: "Introducción al UX Research y UX Writing",
+    institution: "Universidad Católica de Pereira",
+    descriptions: [
+      "Planifique investigaciones centradas en el usuario",
+      "Análisis e Interpretación de Datos",
+      "Aprendí a definir tono y voz de marca",
+      "Apliqué métodos de investigación como encuestas y testeo",
+      "Construí mapas de empatía para entender al usuario"
+    ],
+    link: "https://drive.google.com/file/d/1Hz7hTiqsr4FSG0q3E1AWKK6Fx50LFpdu/view?usp=drive_link",
+    tags: ["UX Research", "UX Writing", "Investigación", "Mapas de empatía", "User persons", "Storytelling"],
+    duration: "Finalizado 05/2024"
+  }
 ];
