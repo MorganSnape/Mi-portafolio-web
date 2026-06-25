b299dc6d98263fc8e8d5f1cdd2a5eab47995d996
morgansnape <gabriellangeles@outlook.com>
2026-06-25 13:11:16 -0500

    Se crea la sección de react

--- src/data/blog/dataSeccionesBlog.ts
+++ src/data/blog/dataSeccionesBlog.ts
@@ -13,5 +13,11 @@ export const dataSeccionesBlog: SectionBlogType[] = [
     description: "El diseño gráfico es la disciplina que combina arte y comunicación para transmitir mensajes específicos de forma visual. Utilizando elementos como imágenes, tipografías, colores y composición, busca resolver problemas y satisfacer necesidades de comunicación entre una marca y su público.",
     image: "/images/blog/portada-D-Grafico.jpeg",
   },
+  {
+    slug: "react",
+    title: "Apuntes sobre React",
+    description: "React es una biblioteca de JavaScript que permite crear interfaces de usuario dinámicas y complejas. Es una de las bibliotecas más populares para el desarrollo de aplicaciones web y se utiliza ampliamente en la industria. React se enfoca en facilitar la creación de interfaces de usuario, permitiendo a los desarrolladores crear aplicaciones web de alta calidad y eficiencia.",
+    image: "/images/blog/portada-React.png",
+  }
 ];
 
--- src/models/index.ts
+++ src/models/index.ts
@@ -126,5 +126,5 @@ export interface SectionBlogType {
   title: string;
   description: string;
   image: string;
-  slug: 'javascript' | 'diseñoGrafico';
+  slug: 'javascript' | 'diseñoGrafico' | 'react';
 }
